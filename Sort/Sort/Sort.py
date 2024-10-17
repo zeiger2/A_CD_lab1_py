e4ffa3f9bc300c228cb7eb247a8555d3b37dc78f
zeiger2 <zverevart@inbox.ru>
2024-10-17 13:18:07 +0300

    0.1

--- Sort/Sort/Sort.py
+++ Sort/Sort/Sort.py
@@ -1,1 +1,44 @@
+from random import *
 
+#Сортировка вставкой
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+#Быстрая сортировка
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr, i, j)
+    swap(arr, i + 1, high)
+    return i + 1
+
+def quickSort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+
+def RandomArr(kol,_seed):
+    seed(_seed)
+    A=[randint(0,50) for _ in range(kol)]
+    return A
+
+A=[]
+A=RandomArr(15,5)
+#print(A)
+#print(insertion_sort(A))
